3be3862ff18394449c838fa4873e856120f3447c
William Schwindt <williammschwindt@gmail.com>
2020-03-08 16:56:51 -0700

    get smurfs action set up

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const FETCHING_SMURFS_START = 'FETCHING_SMURFS_START';
+export const FETCHING_SMURFS_SUCCESS = 'FETCHING_SMURFS_SUCCESS';
+export const FETCHING_SMURFS_FAILURE = 'FETCHING_SMURFS_FAILURE';
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: FETCHING_SMURFS_START });
+
+    axios
+    .get("http://localhost:3333/smurfs")
+    .then(res=> {
+        console.log(res);
+        dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+        console.log(err);
+        dispatch({ type: FETCHING_SMURFS_FAILURE, payload: err});
+    })
+}
