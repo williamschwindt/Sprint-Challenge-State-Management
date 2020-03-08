6a78b2295d3d3ec38d77e9bf65dd2e3a210002b3
William Schwindt <williammschwindt@gmail.com>
2020-03-08 16:49:38 -0700

    store set up

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { reducer } from './reducers/index';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+);
---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    name: '',
+    age: null,
+    height: ''
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default :
+            return state;
+    }
+}
