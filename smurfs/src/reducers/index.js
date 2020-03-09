b3f16e1bbfd151ba62c4b11a71cb3b9d1cefbf17
William Schwindt <williammschwindt@gmail.com>
2020-03-08 17:02:07 -0700

    get smurfs reducer logic

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,11 +1,30 @@
+import { FETCHING_SMURFS_START, FETCHING_SMURFS_SUCCESS, FETCHING_SMURFS_FAILURE } from '../actions/index';
+
 const initialState = {
-    name: '',
-    age: null,
-    height: ''
+    smurfDetails: {},
+    isFetching: false,
+    error: ''
 }
 
 export const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case FETCHING_SMURFS_START :
+            return {
+                ...state,
+                isFetching: true
+            }
+        case FETCHING_SMURFS_SUCCESS : 
+            return {
+                ...state,
+                smurfDetails: action.payload,
+                isFetching: false
+            }
+        case FETCHING_SMURFS_FAILURE : 
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false
+            }
         default :
             return state;
     }
