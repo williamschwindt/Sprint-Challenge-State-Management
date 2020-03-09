0ee86890faceb79134b9dcd7f3099423b22b9cc0
William Schwindt <williammschwindt@gmail.com>
2020-03-08 17:22:46 -0700

    app connected to store

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions/index';
+import { Smurfs } from '../components/Smurfs';
+
+const app = ({ getSmurfs, smurfDetails, isFetching, error}) => {
+  const [ smurf, setSmurf ] = useState({
+    name: '',
+    age: null,
+    height: ''
+  });
+
+  if (isFetching) {
+    return <h2>Finding The Smurfs</h2>
   }
+
+  else if (error !== '') {
+    return <h2>We Couldn't Find The Smurfs, Try Again?</h2>
+  }
+
+  return (
+    <div>
+      <Smurfs />
+      <button onClick={getSmurfs}>See The Smurfs</button>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    smurfDetails: state.smurfDetails,
+    isFetching: state.isFetching,
+    error: state.error
+  }
+
 }
 
 export default App;
